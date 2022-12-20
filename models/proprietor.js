93bfb0552d50320a7caeb0384e7b262cd297346f
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-12-20 23:38:09 +0600

    proprietorSchema model

---
+++ models/proprietor.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const proprietorSchema = new Schema({
+  buildingName: { type: String, required: true },
+  buildingType: { type: String, required: true },
+  seatAvailability: { type: Number, required: true },
+  onlyFor: { type: String, required: true },
+  contact: { type: Number, min: [11, 'Phone number must be eleven digits'] },
+  image: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Proprietor', proprietorSchema);
