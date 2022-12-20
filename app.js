a2199b962ad0d69141375017c3e90aadf078e61e
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-12-21 00:39:44 +0600

    connect to database code insert into app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/toLet').then(() => {
+  console.log('Database connected');
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
