ffdaed4f4b88da4c3b5865a5014694001123f2dd
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-12-21 00:58:04 +0600

    view all mess info by hiting /show route

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+const Proprietor = require('./models/proprietor');
 mongoose.connect('mongodb://localhost:27017/toLet').then(() => {
   console.log('Database connected');
 });
-
+app.get('/show', async (req, res) => {
+  const proprietors = await Proprietor.find({});
+  res.render('mess/index', { proprietors });
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
