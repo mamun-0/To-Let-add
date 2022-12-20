cc0a613af8b4e140bc0d0285d9a425d9806c000f
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-12-21 01:02:21 +0600

    set ejs config

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
 const Proprietor = require('./models/proprietor');
 mongoose.connect('mongodb://localhost:27017/toLet').then(() => {
   console.log('Database connected');
 });
+
+// set view engine for ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/show', async (req, res) => {
   const proprietors = await Proprietor.find({});
   res.render('mess/index', { proprietors });
