ac3ff5b30af8098151755828c6951c0dd7aae9e0
mamun-0 <md.abdullahalmamun200@gmail.com>
2022-12-26 15:57:07 +0600

    add show route and mess prefix inside render a template and also some lines order

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate =  require('ejs-mate')
 const Proprietor = require('./models/proprietor');
+mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/toLet').then(() => {
   console.log('Database connected');
 });
 
-//serving static files
-app.use(express.static(path.join(__dirname, 'public')));
 //middleware
 app.use(express.urlencoded({extended:true}));
+//serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 // set view engine for ejs
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -19,17 +20,23 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/mess', async (req, res) => {
   const proprietors = await Proprietor.find({});
-  res.render('index', { proprietors });
+  res.render('mess/index', { proprietors });
 });
 app.get('/mess/new', (req,res)=>{
-  res.render('newMess');
+  res.render('mess/new');
 });
 app.post('/mess', async (req,res)=>{
   const {mess} = req.body;
   const proprietor = new Proprietor(mess);
   await proprietor.save();
   res.redirect('/mess');
-})
+});
+
+app.get('/mess/:id', async (req, res) => {
+  const { id } = req.params;
+  const proprietor = await Proprietor.findById(id);
+  res.render('mess/show', {proprietor});
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
