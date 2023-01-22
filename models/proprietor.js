386be82348c99b78021185177c8c50a836d52f82
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-01-22 20:14:41 +0600

    remove all extra validation check, that check take care Joi validation

--- models/proprietor.js
+++ models/proprietor.js
@@ -1,17 +1,15 @@
+const { string, number } = require('joi');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const proprietorSchema = new Schema({
-  buildingName: { type: String, required: true },
-  buildingType: { type: String, required: true },
-  seatAvailability: { type: Number, required: true },
-  onlyFor: { type: String, required: true },
-  contact: {
-    type: String,
-    minLength: [11, 'Phone number must be eleven digits'],
-  },
-  image: { type: String, required: true },
-  description: { type: String, required: true },
+  buildingName: String,
+  buildingType: String,
+  seatAvailability: Number,
+  onlyFor: String,
+  contact: String,
+  image: String,
+  description: String,
 });
 
 module.exports = mongoose.model('Proprietor', proprietorSchema);
