f7520b8b8fdb13d21e40a98f29814d7858649b1c
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-01-22 20:30:17 +0600

    add valid method for buildingType and onlyFor property

---
+++ JoiSchema/proprietorValidator.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+
+module.exports.validateProprietorSchema = Joi.object({
+  mess: Joi.object({
+    buildingName: Joi.string().required(),
+    buildingType: Joi.string().valid('Mess','Flat').required(),
+    seatAvailability: Joi.number().min(0).required(),
+    onlyFor : Joi.string().valid('Male','Female').required(),
+    contact : Joi.string().required(),
+    image:Joi.string().required(),
+    description : Joi.string().required()
+  }).required(),
+});
