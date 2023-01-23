1147e0c5a2244fe99b2268791427845ca51a5b45
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-01-23 16:08:37 +0600

    add express router

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate =  require('ejs-mate')
-const Proprietor = require('./models/proprietor');
 const methodOverride = require('method-override');
-const wrapAsync = require('./utils/wrapAsync');
+const messRouter = require('./router/mess');
 const AppError = require('./utils/AppError');
-const {phone} = require('phone');
-const { proprietorSchema } = require('./middleware/middleware');
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/toLet').then(() => {
   console.log('Database connected');
@@ -24,43 +21,9 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/mess', wrapAsync(async (req, res) => {
-  const proprietors = await Proprietor.find({});
-  res.render('mess/index', { proprietors });
-}));
-app.get('/mess/new', (req,res)=>{
-  res.render('mess/new');
-});
-app.post('/mess', proprietorSchema, wrapAsync(async (req,res)=>{
-  const {mess} = req.body;
-  const isValidNum = phone(mess.contact,{cuntry:null});
-  if(!isValidNum.isValid){throw new AppError(400, "Invalid phone number");}
-  const proprietor = new Proprietor(mess);
-  await proprietor.save();
-  res.redirect('/mess');
-}));
 
-app.get('/mess/:id', wrapAsync(async (req, res) => {
-  const { id } = req.params;
-  const proprietor = await Proprietor.findById(id);
-  res.render('mess/show', {proprietor});
-}));
-app.get('/mess/:id/edit',wrapAsync(async (req,res)=>{
-  const {id} = req.params;
-  const proprietor = await Proprietor.findById(id);
-  res.render('mess/edit',{proprietor});
-}));
-app.put('/mess/:id', wrapAsync(async (req,res)=>{
-  const {mess} = req.body;
-  const {id} = req.params;
-  await Proprietor.findByIdAndUpdate(id, {...mess});
-  res.redirect(`/mess/${id}`);
-}));
-app.delete('/mess/:id', wrapAsync(async (req,res)=>{
-  const {id} = req.params;
-  await Proprietor.findByIdAndDelete(id);
-  res.redirect('/mess');
-}));
+app.use('/mess', messRouter);
+
 app.all('*',(req,res,next)=>{
   next(new AppError(404, "Page not found"));
 });
---
+++ router/mess.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const { phone } = require('phone');
+const wrapAsync = require('../utils/wrapAsync');
+const Proprietor = require('../models/proprietor');
+const AppError = require('../utils/AppError');
+const { proprietorSchema } = require('../middleware/middleware');
+
+router.get('/', wrapAsync(async (req, res) => {
+    const proprietors = await Proprietor.find({});
+    res.render('mess/index', { proprietors });
+  })
+);
+
+router.get('/new', (req, res) => {
+  res.render('mess/new');
+});
+
+router.post(
+  '/',
+  proprietorSchema,
+  wrapAsync(async (req, res) => {
+    const { mess } = req.body;
+    const isValidNum = phone(mess.contact, { cuntry: null });
+    if (!isValidNum.isValid) {
+      throw new AppError(400, 'Invalid phone number');
+    }
+    const proprietor = new Proprietor(mess);
+    await proprietor.save();
+    res.redirect('/mess');
+  })
+);
+
+router.get(
+  '/:id',
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const proprietor = await Proprietor.findById(id);
+    res.render('mess/show', { proprietor });
+  })
+);
+router.get(
+  '/:id/edit',
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const proprietor = await Proprietor.findById(id);
+    res.render('mess/edit', { proprietor });
+  })
+);
+router.put(
+  '/:id',
+  wrapAsync(async (req, res) => {
+    const { mess } = req.body;
+    const { id } = req.params;
+    await Proprietor.findByIdAndUpdate(id, { ...mess });
+    res.redirect(`/mess/${id}`);
+  })
+);
+router.delete(
+  '/:id',
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    await Proprietor.findByIdAndDelete(id);
+    res.redirect('/mess');
+  })
+);
+
+
+module.exports = router;
