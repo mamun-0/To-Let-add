e6af43c807563588de340ac12a5d7ddaa6d5162d
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-01-23 17:06:56 +0600

    configure express-session

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejsMate =  require('ejs-mate')
 const methodOverride = require('method-override');
 const messRouter = require('./router/mess');
 const AppError = require('./utils/AppError');
+const session =  require('express-session');
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/toLet').then(() => {
   console.log('Database connected');
@@ -14,13 +15,22 @@ mongoose.connect('mongodb://localhost:27017/toLet').then(() => {
 //middleware
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(session({
+  secret:"thisisoursillySecret",
+  resave:true,
+  saveUninitialized: true,
+  cookie:{
+    httpOnly:true,
+    expires: Date.now() + 7*24*60*60*1000,
+    maxAge: 7*24*60*60*1000
+  }
+}));
 //serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 // set view engine for ejs
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 
 app.use('/mess', messRouter);
 
