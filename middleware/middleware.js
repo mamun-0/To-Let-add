bb1b894f18731d12024d1ea74aad70b7da8d06a3
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-01-25 16:17:00 +0600

    isLoggedin and validPhone middleware is added

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,5 +1,6 @@
 const {validateProprietorSchema} = require('../JoiSchema/proprietorValidator');
 const AppError = require('../utils/AppError');
+const {phone} = require('phone');
 
 module.exports.proprietorSchema = (req,res,next)=>{
     const { error } = validateProprietorSchema.validate(req.body);
@@ -9,4 +10,22 @@ module.exports.proprietorSchema = (req,res,next)=>{
     }else{
         next();
     }
+}
+module.exports.isLoggedin = (req,res,next)=>{
+  if(!req.isAuthenticated()){
+    req.flash('error', 'You must login first.');
+    return res.redirect('/user/login');
+  }else{
+    next();
+  }
+}
+module.exports.validPhone = (req,res,next)=>{
+  const {mess} = req.body;
+  const isValidNum = phone(mess.contact, { cuntry: null });
+  console.log(req.originalUrl);
+  if (!isValidNum.isValid) {
+    req.flash('error', 'Invalid phone number');
+    return res.redirect(req.originalUrl);
+  }
+  next();
 }
