165f065b978e5ff21dc0a04b5f5d3f275078a1ad
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-01-26 23:57:10 +0600

    author property add

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -9,6 +9,7 @@ const proprietorArr = [
   {
     buildingName: 'Doctor Bari Mess',
     buildingType: 'Mess',
+    author: '63d21c9b06c88b264c356351',
     seatAvailability: 21,
     onlyFor: 'Male',
     contact: '+8801798668299',
@@ -20,6 +21,7 @@ const proprietorArr = [
   {
     buildingName: 'White House Mess',
     buildingType: 'Mess',
+    author: '63d21c9b06c88b264c356351',
     seatAvailability: 30,
     onlyFor: 'Male',
     contact: '+8801798668299',
@@ -31,6 +33,7 @@ const proprietorArr = [
   {
     buildingName: 'Green House Mess',
     buildingType: 'Mess',
+    author: '63d21c9b06c88b264c356351',
     seatAvailability: 13,
     onlyFor: 'Male',
     contact: '+8801798668299',
@@ -42,6 +45,7 @@ const proprietorArr = [
   {
     buildingName: 'Walton Building',
     buildingType: 'Flat',
+    author: '63d21c9b06c88b264c356351',
     seatAvailability: 17,
     onlyFor: 'Male',
     contact: '+8801798668299',
@@ -51,15 +55,15 @@ const proprietorArr = [
       'Good mess for medium or specialy for poor people. Thanks for owner',
   },
   {
-    buildingName: 'Unknown Mess',
+    buildingName: 'Blue House',
     buildingType: 'Mess',
+    author: '63d21c9b06c88b264c356351',
     seatAvailability: 14,
-    onlyFor: 'Female',
-    contact: '+8801798668299',
+    onlyFor: 'Male',
+    contact: '+8801798668288',
     image:
-      'https://images.unsplash.com/photo-1672106157109-056c567e3833?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=544&q=80',
-    description:
-      'Good mess for medium or specialy for poor people. Thanks for owner',
+      'https://images.unsplash.com/photo-1674421338672-3591ba6f176e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80',
+    description: `Blue House is named as the house of south korea's prime minister house.`,
   },
 ];
 
