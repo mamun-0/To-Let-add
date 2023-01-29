6cbca7fe3d903e9fa37e6ec0523f66caed1346c7
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-01-29 22:37:16 +0600

    change color of 404 text all time jsvascript code

---
+++ public/javaScript/colorChanger.js
@@ -0,0 +1,13 @@
+const textDiv = document.querySelectorAll('.textDiv');
+const makeColor = function () {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+for (let x of textDiv) {
+  setInterval(() => {
+    x.style.color = makeColor();
+  }, 500);
+}
+console.log('Its working')
