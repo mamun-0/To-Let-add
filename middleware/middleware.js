d6fbb1a533ca956450091a5ed0ef7d238063121c
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-05-29 21:38:25 +0600

    replace phone package to custom regEx phone

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,34 +1,37 @@
-const {validateProprietorSchema} = require('../JoiSchema/proprietorValidator');
+const {
+  validateProprietorSchema,
+} = require('../JoiSchema/proprietorValidator');
 const AppError = require('../utils/AppError');
-const {phone} = require('phone');
+// const {phone} = require('phone');
+const { phone } = require('../utils/regExNumber');
 const Proprietor = require('../models/proprietor');
-module.exports.proprietorSchema = (req,res,next)=>{
-    const { error } = validateProprietorSchema.validate(req.body);
-    if (error) {
-      const msg = error.details.map((el) => el.message).join(',');
-      throw new AppError(400, msg);
-    }else{
-        next();
-    }
-}
-module.exports.isLoggedin = (req,res,next)=>{
-  if(!req.isAuthenticated()){
+module.exports.proprietorSchema = (req, res, next) => {
+  const { error } = validateProprietorSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new AppError(400, msg);
+  } else {
+    next();
+  }
+};
+module.exports.isLoggedin = (req, res, next) => {
+  if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
     req.flash('error', 'You must login first.');
     return res.redirect('/user/login');
-  }else{
+  } else {
     next();
   }
-}
-module.exports.validPhone = (req,res,next)=>{
-  const {mess} = req.body;
-  const isValidNum = phone(mess.contact, { cuntry: null });
+};
+module.exports.validPhone = (req, res, next) => {
+  const { mess } = req.body;
+  const isValidNum = phone(mess.contact);
   if (!isValidNum.isValid) {
     req.flash('error', 'Invalid phone number');
     return res.redirect(req.originalUrl);
   }
   next();
-}
+};
 
 module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
