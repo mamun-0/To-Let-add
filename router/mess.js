8f4f813d7a26f7d5586f0ef754c0a51bfbb9a0ed
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-07-13 11:17:33 +0600

    remove phone validation

--- router/mess.js
+++ router/mess.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require('../utils/wrapAsync');
 const Proprietor = require('../models/proprietor');
 const { isLoggedin } = require('../middleware/middleware');
-const { proprietorSchema, validPhone, isAuthor } = require('../middleware/middleware');
+const { proprietorSchema, isAuthor } = require('../middleware/middleware');
 const multer = require('multer');
-const {storage, cloudinary} = require('../cloudinary/cloudinary');
-const upload = multer({storage});
+const { storage, cloudinary } = require('../cloudinary/cloudinary');
+const upload = multer({ storage });
 
-router.get('/', wrapAsync(async (req, res) => {
+router.get(
+  '/',
+  wrapAsync(async (req, res) => {
     const proprietors = await Proprietor.find({});
     res.render('mess/index', { proprietors });
   })
@@ -23,12 +25,11 @@ router.post(
   isLoggedin,
   upload.single('image'),
   proprietorSchema,
-  validPhone,
   wrapAsync(async (req, res) => {
     const { mess } = req.body;
-    const {path, filename} = req.file;
+    const { path, filename } = req.file;
     const proprietor = new Proprietor(mess);
-    proprietor.image = {path, filename};
+    proprietor.image = { path, filename };
     proprietor.author = req.user._id;
     await proprietor.save();
     req.flash('success', 'New mess created successfully.');
@@ -59,14 +60,13 @@ router.put(
   isLoggedin,
   upload.single('image'),
   isAuthor,
-  validPhone,
   wrapAsync(async (req, res) => {
     const { mess } = req.body;
-    const {filename, path} = req.file;
+    const { filename, path } = req.file;
     const { id } = req.params;
     const { image } = await Proprietor.findById(id);
     await cloudinary.uploader.destroy(image.filename);
-    mess.image = {filename, path};
+    mess.image = { filename, path };
     await Proprietor.findByIdAndUpdate(id, mess);
     req.flash('success', 'successfully edited.');
     res.redirect(`/mess/${id}`);
@@ -78,13 +78,12 @@ router.delete(
   isAuthor,
   wrapAsync(async (req, res) => {
     const { id } = req.params;
-    const {image} = await Proprietor.findById(id);
+    const { image } = await Proprietor.findById(id);
     await cloudinary.uploader.destroy(image.filename);
     await Proprietor.findByIdAndDelete(id);
     req.flash('success', 'successfully deleted.');
     res.redirect('/mess');
   })
 );
-
 
 module.exports = router;
--- utils/regExNumber.js
+++
@@ -1,6 +0,0 @@
-const bdPhoneRegEx = /^(\+?880|0)1[3456789]\d{8}$/g;
-function phone(phoneNumber) {
-  return bdPhoneRegEx.test(phoneNumber);
-}
-
-module.exports = { phone };
